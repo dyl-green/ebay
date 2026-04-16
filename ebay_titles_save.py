cd3cc8895ae4a9bacf453e5758b1bea793cd543c
dyl-green <drosser978@gmail.com>
2026-04-16 13:05:13 -0400

    add titles

---
+++ ebay_titles_save.py
@@ -0,0 +1,151 @@
+'''
+
+
+
+
+Longchamp Le Pliage Depose “Shopping” Tote Foldable Handbag Purse Tan Beige
+
+"Three Vintage 2005 Apple Cinema HD Display 23-inch, Model A1082, NOT TESTED"
+
+"Vintage 2005 Apple Cinema HD Display 23-inch, Model A1082, w/ Power NOT TESTED"
+
+University of California Berkeley Heather Grey Sweatshirt Size SM Boxy Spellout
+
+Rock & Republic Jeans 38x30 Blue Dark Wash Denim Y2K Embroidered Flared
+
+Pendleton Button Up Shirt Plaid Flannel Men’s Size Medium Yellow Cotton
+
+Akademiks Jeans 46x32 Light Wash Denim Vintage Y2k Baggy Skater Hip Hip Rap
+
+90s Vintage Starter Sweatshirt Mens Large 50/50 V-Stitch Blank Baggy Y2K
+
+The North Face A5 Series Track Jacket Mens Size 5XL Black Gray Striped Full Zip
+
+Original Quality Wired Apple Earbuds (lightning)
+
+Nike On Field Washington Redskins Ryan Kerrigan #91 Jersey Burgundy Mens Size XL
+
+Timberland Heritage 6” Waterproof Boot Wheat Nubuck Women’s 9.5 Rebolt Edition
+
+Vintage Ramses The Great Pharaoh Egypt Graphic T-shirt Men’s XL Travel Africa
+
+NFL New York Jets Long Sleeve Shirt Mens Medium On Field Graphic print
+
+Vintage Levi's 550 Relaxed Fit Jeans Men’s 33x34 Dark Wash Indigo NWT USA
+
+Playboy Sweatshirt Black White Men’s Medium
+
+Vintage NFL Pro Line Chicago Bears Dick Butkus #51 Jersey - Men's Size Large
+
+Vintage Patagonia Pima Cotton Shirt Button Up Red Plaid Organic Men’s Large
+
+Polo Ralph Lauren V-Neck Sweater Green Mens Large Cotton Knit
+
+Avirex Zip Up Hoodie Vintage 90s Y2K Pullover Dark Grey Mens Size 2XL Baggy
+
+Calvin Klein Jacket Black Women’s Large Full Zip Long Sleeve
+
+Vintage Nike Jacket Mens 2XL Big & Tall Basketball Full Zip Tech
+
+Manchester City Long Sleeve Shirt Boys 12-13 Premier League Soccer
+
+Mens Harley Davidson Biker Long Sleeve Shirt 2XL HD Performance
+
+The North Face Jacket Women’s full zip waterproof Adjustable Black Size Small
+
+Blue L.L. Bean Jacket Women’s Medium Waterproof Hooded Pockets
+
+Vintage 90s Nature T-shirt Single Stitched Men’s Size Medium Made In USA
+
+Dr. Martens Air Wair Youth Size 1 US White Leather Lace Up Side Zip Boots 1460J
+
+Vintage 90s Patagonia Full Zip Fleece Jacket Mens XXL Black
+
+Rocawear Polo Shirt Mens 2XL Red Striped Embroidered R Logo Y2K
+
+Cole Haan 5.Zerogrand Shoes Men’s Size 12 All Terrain Running
+
+Levi’s 501 Denim Jeans 25x27 Blue Light Wash Ribcage Straight Ankle Button Fly
+
+Nike Mercurial X Proximo Black Youth 5.5Y Soccer Cleats
+
+Vintage Norm Thompson Fishing Jacket Men’s XL Tan Vented Utility Multi Pocket
+
+Y2K Vintage Southpole Jeans Mens 38x30 Hip Hop Grunge Skater Light Wash
+
+Vintage Ralph Lauren Cargo Pants Women 8 Tan Multi Pocket Military Style Utility
+
+Vintage 70s Striped Tiered Tent Mini Dress Cotton Poplin Sleeveless Blue White
+
+Carhartt Relaxed Fit Work Pants Men’s 38x30 Brown Stretch Utility Workwear
+
+Vintage 90s Blue Nike Track Pants Men's Large
+
+Y2K Neeso Flare Jeans Women's Size 9 Rhinestone Distressed Denim 32x30
+
+White Vintage Rocawear Shirt Mens XL Button Up Geometric Embroidered Logo Baggy
+
+VINTAGE Aeropostale Shirt Navy Blue Mens Size Large Medic Graphic Print
+
+Vintage 80s L.L. Bean Knit Mock Neck Sweater Full Zip White Women’s Large
+
+Zoo York T-Shirt White Mens Big & Tall XL Big Graphic Spellout Vintage Skate
+
+VINTAGE Planet Hollywood Sweatshirt Mens Large Black Raglan Knit Fleece Heavy
+
+University of Illinois Shirt NCAA Basketball Mens 2XL Y2K Orange
+
+MLB Pittsburgh Pirates Long Sleeve Shirt Black Men's Large 2007
+
+Vintage brown Embroidered rhinestone Y2K knockout Brand button up shirt Size Xl
+
+Nike Tiger Woods Air Zoom TW 2011 Golf Shoes Black Red Leather Mens 14 Spiked
+
+Nike Rise React Flyknit Thunder Grey Women's Running Shoes AV5553-004 Size 7.5
+
+Calvin Klein Mini Dress Black Women’s Size 8P Sleeveless Flare
+
+Buck Gear 3D Print T-Shirt Mens Large Black Grunge Deer Hunting Camo
+
+Ecko Unlimited Camo Shorts Men’s Size Large Y2K Pockets
+
+Mitchell And Ness Nylon Shorts Phillies MLB Women’s Medium Platinum Silver
+
+Vintage 80s Nike Leather Shoes Women’s 7.5 White Black Swoosh Adjustable
+
+Vintage 90s Sierra Designs Vest Grey Sleeveless Fleece Full Zip Men’s Medium
+
+Vintage Barack Obama 44th President 2008 Victory Rap Tee Shirt Y2K Black Sz L
+
+Vintage Crochet Knit Vest Size Small 38 Red Yellow Teal The Villager Striped
+
+Vintage Polo Ralph Lauren Red Hooded Vented Jacket Mens XL Full Zip Classic Pony
+
+PSG T-Shirt Navy Blue Men’s Size XS Parc Des Princes Ligue 1 Soccer Futbol
+
+"Women's L.L.Bean 8"" Duck Boots Size 7 | Waterproof Leather Brown Tan Rain Boots"
+
+Black Harry Styles Hoodie Tour Band Music Size Medium
+
+Arsenal F.C. Jacket Full Zip Away Blue 2016/17 Premier League
+
+Y2K Brown Amsterdam Unlimited Script T-shirt Men’s XXL Short Sleeve
+
+Vintage Reebok On-Field Washington Redskins Beanie Hat Men’s OS NFL Embroidered
+
+Vintage Philadelphia Eagles 2004 NFC Conference Championship Men’s Size 2XL
+
+Aaliyah Rap T-shirt Women’s Small Graphic Print
+
+"Bubba Gump ""Boat"" t-shirt Men’s XL Blue Graphic Print"
+
+Philadelphia Phillies T-Shirt Men's 2XL Y2K 2008 MLB National League Champions
+
+Air Jordan T-Shirt Black Mens Size XL Graphic Print Athletic Casual
+
+Nike Hat Washington Huskies Purple Gold
+
+Biederlack NCAA Notre Dame Fightin’ Irish Game Day Throw Blanket
+
+
+'''
